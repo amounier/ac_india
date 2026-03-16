3e3173d2579517bcc03f2808ff21dc223bb27eca
amounier <andre.mounier@ens-paris-saclay.fr>
2026-03-16 15:00:34 +0100

    new python files for climate data formatting and other utility funcs

---
+++ utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  9 17:57:23 2026
+
+@author: amounier
+"""
+
+import time
+
+
+
+def main():
+    tic = time.time()
+    
+    
+    tac = time.time()
+    print('Done in {:.2f}s.'.format(tac-tic))
+    
+if __name__ == '__main__':
+    main()
